b1e32ce93512bee46259e285bc5ac9b9822f755f
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2023-06-02 19:35:46 -0300

    nova atividade

---
+++ handwriting_recognition_torch/words_recognition_with_pyTorch.py
@@ -0,0 +1,77 @@
+# importação dos dados
+import os
+import tarfile
+from tqdm import tqdm
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+
+import torch
+import torch.optim as optim
+from torchsummaryX import summary
+
+from mltu.torch.model import Model
+from mltu.torch.losses import CTCLoss
+from mltu.torch.dataProvider import DataProvider
+from mltu.torch.metrics import CERMetric, WERMetric
+from mltu.torch.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, Model2onnx, ReduceLROnPlateau
+
+from mltu.preprocessors import ImageReader
+from mltu.transformers import ImageResizer, LabelIndexer, LabelPadding, ImageShowCV2
+from mltu.augmentors import RandomBrightness, RandomRotate, RandomErodeDilate, RandomSharpen
+
+from model import Network
+from configs import ModelConfigs
+
+
+dataset, vocab, max_len = [], set(), 0
+dataset_path = "data\\iam_data\\"
+# Preprocess the dataset by the specific IAM_Words dataset file structure
+if os.path.exists(dataset_path):
+    words = open(f"{dataset_path}words.txt", "r").readlines()
+    for line in tqdm(words):
+        if line.startswith("#"):
+            continue
+
+        line_split = line.split(" ")
+        if line_split[1] == "err":
+            continue
+
+        folder1 = line_split[0][:3]
+        folder2 = "-".join(line_split[0].split("-")[:2])
+        file_name = line_split[0] + ".png"
+        label = line_split[-1].rstrip('\n')
+        rel_path = f"{dataset_path}/words/{folder1}/{folder2}/{file_name}"
+        if not os.path.exists(rel_path):
+            print(f"File not found: {rel_path}")
+            continue
+
+        dataset.append([rel_path, label])
+        vocab.update(list(label))
+        max_len = max(max_len, len(label))
+else:
+    print("caminho nao encontrado")
+
+configs = ModelConfigs()
+
+# Save vocab and maximum text length to configs
+configs.vocab = "".join(sorted(vocab))
+configs.max_text_length = max_len
+configs.save()
+
+# Create a data provider for the dataset
+data_provider = DataProvider(
+    dataset=dataset,
+    skip_validation=True,
+    batch_size=configs.batch_size,
+    data_preprocessors=[ImageReader(image_class="PIL")],
+    transformers=[
+        ImageShowCV2(),  # uncomment to show images during training
+        ImageResizer(configs.width, configs.height, keep_aspect_ratio=False),
+        LabelIndexer(configs.vocab),
+        LabelPadding(max_word_length=configs.max_text_length,
+                     padding_value=len(configs.vocab))
+    ],
+    use_cache=True,
+    workers=1
+)
